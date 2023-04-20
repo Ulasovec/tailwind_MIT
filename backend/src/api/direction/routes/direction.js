f89e485ce4d280d74a6b44c601f00a39685fff3b
Ulasovec <ulasovec-kirill@mail.ru>
2023-04-20 10:21:10 +0300

    registration

--- backend/src/api/direction/routes/direction.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * direction router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::direction.direction');
--- backend/src/api/direction/services/direction.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * direction service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::direction.direction');
